2d7e8f927e0f68549e62dbf9c47a760ed5736811
Farhan Yuda Pahlevi <pahlevi.kun@gmail.com>
2018-03-03 14:37:53 +0700

    Check 1

--- Emoji/ApaYaViewController.swift
+++ Emoji/ApaYaViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 
 class ApaYaViewController: UIViewController {
     
+    let emojis: [String] = ["🍆","🍅"]
+  
+    @IBOutlet var cardButton: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let index = cardButton.index(of: sender){
+            let emojis = getEmoji(fromIndex: index)
+            let card = cardButton[index]
+            card.setTitle(emojis, for: .normal)
+            card.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+            print(index)
+        }else{
+            fatalError("This card is not included in cardButtons")
+        }
+    }
+    func getEmoji(fromIndex index:Int) -> String{
+        if index % 2 == 0{
+            return emojis[0]
+        }else{
+            return emojis[1]
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
